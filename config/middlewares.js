b9a8f777a93bfac46993b8a91dff80cd9998f5d8
rafifthi <rafiffathii@gmail.com>
2022-09-03 17:57:23 +0700

    Cloudinary config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,18 @@
 module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
